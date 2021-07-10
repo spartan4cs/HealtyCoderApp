e1507007ebc44bb078275a335484bb3cb77eb46c
aakash <aakashkumbhare@gmail.com>
2021-07-10 13:11:27 +0530

    Update

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,6 +1,13 @@
 package com.healthycoderapp;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -19,6 +26,7 @@ class BMICalculatorTest {
 		assertTrue(recommended);
 	}
 
+	@Test
 	void should_return_false_when_diet_recommended() {
 		// given
 		double weight = 50.0;
@@ -30,6 +38,7 @@ class BMICalculatorTest {
 		assertFalse(recommended);
 	}
 
+	@Test
 	void should_ThrowExceptionWhenHeight_zero() {
 		// given
 		double weight = 50.0;
@@ -39,6 +48,23 @@ class BMICalculatorTest {
 		Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
 		// then
 		assertThrows(ArithmeticException.class, executable);
+
+	}
+
+	@Test
+	void shouldReturnCoder_With_WorstBMI_WhenListNotempty() {
+
+		// given
+		List<Coder> coders = Arrays.asList(new Coder(1.80, 60.0), new Coder(1.82, 98.0), new Coder(1.82, 64.7));
+
+		// when
+
+		Coder coderwithWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+
+		// then
+
+		assertAll(() -> assertEquals(1.82, coderwithWorstBMI.getHeight()),
+				() -> assertEquals(98.0, coderwithWorstBMI.getWeight()));
 
 	}
 }
